65a758bd5efbf720d8e992f975a960c926e99266
Totktosmog <totktosmog513@gmail.com>
2024-02-24 09:08:04 +0300

    Добавьте файлы проекта.

---
+++ C_Sharp_LINQ_lab_2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C_Sharp_LINQ_lab_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
