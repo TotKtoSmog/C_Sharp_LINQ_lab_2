154ffe58cbb555e0bd562776a866670f4266ec63
Totktosmog <totktosmog513@gmail.com>
2024-02-29 18:52:21 +0300

    Задание № 4 выполнено

---
+++ C_Sharp_LINQ_lab_2/Class/Client.cs
@@ -0,0 +1,18 @@
+﻿
+namespace C_Sharp_LINQ_lab_2.Class
+{
+    internal class Client
+    {
+        internal int Id;
+        internal int Year;
+        internal int Month;
+        internal int Hour;
+        internal Client(int id, int Year, int Month, int Hour)
+        {
+            this.Id = id;
+            this.Year = Year;
+            this.Month = Month;
+            this.Hour = Hour;
+        }
+    }
+}
--- C_Sharp_LINQ_lab_2/Program.cs
+++ C_Sharp_LINQ_lab_2/Program.cs
@@ -9,7 +9,8 @@ namespace C_Sharp_LINQ_lab_2
         {
             //ShowList(Task1(new List<string>() { "A2", "A1", "A3" }, new List<string>() { "B1", "B2", "B3" }));
             //ShowList(Task2(new List<int>() {1,2,3,4,5 }, new List<int>() {11,19,21,31,55 }));
-            ShowList(Task3(GetDataDepartment(), GetDataEmployees()));
+            //ShowList(Task3(GetDataDepartment(), GetDataEmployees()));
+            ShowList(Task4(GetDataClients(), 3));
         }
         static void ShowList<T>(List<T> list)
         {
@@ -61,7 +62,28 @@ namespace C_Sharp_LINQ_lab_2
         static List<string> Task3(List<Department> departments, List<Employee> employees)
             => departments.Join(employees, d => d.Id_Department, e => e.Id_department, (d, e) 
                 => new { Name = d.Name_Department, salary = e.Salary}).GroupBy(res => res.Name).
-            Select(res => $"{res.Key} - {Math.Round(res.Average(n => n.salary),2)}").ToList(); 
+            Select(res => $"{res.Key} - {Math.Round(res.Average(n => n.salary),2)}").ToList();
+        /// <summary>
+        /// Дано целое число K – код одного из клиентов фитнес-центра. Исходная
+        /// последовательность содержит сведения о клиентах этого фитнес-центра.Каждый элемент
+        /// последовательности включает следующие целочисленные поля:
+        /// <Код клиента> <Год> <Номер месяца>
+        /// <Продолжительность занятий(в часах)>
+        /// Для каждого года, в котором клиент с кодом K посещал центр, определить месяц, в
+        /// котором продолжительность занятий данного клиента была наименьшей для данного года
+        /// (если таких месяцев несколько, то выбирать первый из этих месяцев в исходном наборе;
+        /// месяцы с нулевой продолжительностью занятий не учитывать). Сведения о каждом годе
+        /// выводить на новой строке в следующем порядке: наименьшая продолжительность
+        /// занятий, год, номер месяца.Упорядочивать сведения по возрастанию продолжительности
+        /// занятий, а при равной продолжительности – по возрастанию номера года. Если данные о
+        /// клиенте с кодом K отсутствуют, то записать в результирующий файл строку «Нет данных»
+        /// </summary>
+        /// <param name="clients">Лист клиентов</param>
+        /// <param name="k">Код клиента</param>
+        /// <returns></returns>
+        static List<string> Task4(List<Client> clients, int k)
+            => clients.Where(c => c.Id == k).GroupBy(c => c.Year).Select(c => new { tr = c.MinBy(b => b.Hour) }).
+            OrderBy(c => c.tr.Month).Select(c => $"{c.tr.Hour} {c.tr.Year} {c.tr.Hour}").DefaultIfEmpty("Нет данных").ToList();
         static List<Department> GetDataDepartment()
             => new List<Department>()
             {
@@ -82,7 +104,19 @@ namespace C_Sharp_LINQ_lab_2
                 new Employee(8, "Меньшов", "Никанор", 24000, 3),
                 new Employee(9, "Вихирева", "Прасковья", 20060, 3),
             };
-        
+        static List<Client> GetDataClients()
+            => new List<Client>() 
+            { 
+                new Client(1,2012,1,5),
+                new Client(1,2012,2,15),
+                new Client(1,2012,3,2),
+                new Client(1,2013,1,5),
+                new Client(1,2016,1,8),
+                new Client(1,2019,1,1),
+                new Client(1,2019,2,1),
+                new Client(2,3000,1,5),
+                new Client(2,3000,2,8),
+            };
 
 
     }
